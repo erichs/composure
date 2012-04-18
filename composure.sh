d7596bd1b4b77c9ce0d4f89c10e83240aad8fc73
Erich Smith <heartquest@gmail.com>
2012-04-18 13:40:50 -0400

    refactor for dual-use

--- composure.sh
+++ composure.sh
@@ -1,11 +1,55 @@
-export EDITOR=vi
+#!/bin/bash
 
-cull ()
-{ local name=$1
-  eval 'function ' $name ' { ' $(fc -ln -1) '; }'
+source_composure() {
+  export EDITOR=vi
+
+  bind '"\C-j": edit-and-execute-command'
+
+  last_cmd() { fc -ln -1; }
+
+  name ()
+  { local name=$1
+    eval 'function ' $name ' { ' $(last_cmd) '; }'
+  }
+
+  write ()
+  { local func=$1
+    declare -f $func
+  }
+
+  alias r='fc -s'
+  alias sl='eval sudo $(last_cmd)'
 }
 
-write ()
-{ local func=$1
-  declare -f $func
+install_composure() {
+  echo 'stay calm. installing composure elements...'
+
+  # find our absolute PATH
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+  # make Vim automatically chmod +x for files with #! lines
+  done_previously() { [ ! -z "$(grep BufWritePost | grep bin | grep chmod)" ]; }
+
+  if ! $(<~/.vimrc done_previously); then
+    echo 'vimrc: adding automatic chmod+x for files with shebang (#!) lines...'
+    echo 'au BufWritePost * if getline(1) =~ "^#!" | if getline(1) =~ "/bin/" | silent execute "!chmod a+x <afile>" | endif | endif' >> ~/.vimrc
+  fi
+
+  # source this file in .bashrc
+  done_previously() { [ ! -z "$(grep source | grep $DIR | grep composure)" ]; }
+
+  if ! $(<~/.bashrc done_previously) && ! $(<~/.bash_profile done_previously); then
+    echo 'sourcing composure from .bashrc...'
+    echo "source $DIR/$(basename $0)" >> ~/.bashrc
+  fi
+
+  echo 'composure installed.'
 }
+
+if [[ "$BASH_SOURCE" == "$0" ]]; then
+  install_composure
+else
+  source_composure
+fi
