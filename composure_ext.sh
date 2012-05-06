c65ab9a40a1f1b5072f8d463d6940236a4d0a442
Erich Smith <heartquest@gmail.com>
2012-05-05 22:37:06 -0400

    second-order functions
    composure_ext.sh: second-order functions for composure
    
    functions related to metadata queries, git repository queries, or any
    function *about* composure functions in some way, should go here.

---
+++ composure_ext.sh
@@ -0,0 +1,70 @@
+# Second-order functions for composure
+
+all_groups ()
+{
+    about displays all unique metadata groups
+    group composure_ext
+
+    typeset func
+    typeset file=$(mktemp /tmp/composure.XXXX)
+    for func in $(listfunctions);
+    do
+        metafor $func group >> $file
+    done
+    cat $file | sort | uniq
+    rm $file
+}
+
+composed_functions ()
+{
+    about list all functions stored in ~/.composure repository
+    group composure_ext
+
+    typeset f
+    for f in ~/.composure/*.inc
+    do
+        echo ${f%.inc}
+    done | awk -F'/' '{print $NF}'
+}
+
+list_functions_by_group ()
+{
+    about lists functions belonging to a given group
+    param 1: group name
+    example $ list_functions_by_group tools
+    group composure_ext
+
+    glossary $1 | cut -d' ' -f 1
+}
+
+recompose ()
+{
+    about loads a stored function from ~/.composure repo
+    param 1: name of function
+    example $ load myfunc
+    group composure_ext
+
+    source ~/.composure/$1.inc
+}
+
+recompose_all ()
+{
+    about loads all stored functions from ~/.composure repo
+    group composure_ext
+
+    typeset func
+    for func in $(composed_functions)
+    do
+        load $func
+    done
+}
+
+write ()
+{
+    about writes function definition to stdout
+    param 1: name of function
+    example $ write myfunc
+    group composure_ext
+
+    typeset -f $1
+}
