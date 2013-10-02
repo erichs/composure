ef4f5f0478a0741d522aa46646f4f860ac8110ca
Erich Smith <heartquest@gmail.com>
2013-10-02 17:05:11 -0400

    Clean up test script

--- test/t/plumbing.sh
+++ test/t/plumbing.sh
@@ -2,29 +2,15 @@
 . ./wvtest.sh
 . ../composure.sh
 
-_max_letterpress_width
 WVSTART "_composure_keywords"
 WVPASS [ $(_composure_keywords | wc -w) -ge 6 ]
-#WVPASS true
-#WVPASS true
-#WVFAIL false
-#WVPASSEQ "$(ls | sort)" "$(ls)"
-#
-#WVSTART another test
-#(echo nested fail; false); WVFAILRC $?
-#WVPASS true
-#WVPASSNE "5" "5 "
-#WVPASSEQ "" ""
-#(echo nested test; true); WVPASSRC $?
 
-_max_letterpress_width
 WVSTART "_letterpress"
 WVPASSEQ "$(_letterpress foo)" "                    foo"
 WVPASSEQ "$(_letterpress foo bar)" "bar                 foo"
 WVPASSEQ "$(_letterpress foo bar 10)" "bar       foo"
 WVPASSEQ "$(_letterpress foo bar)" "bar                 foo"
 
-_max_letterpress_width
 WVSTART "_typeset_functions"
 typeset count=$(_typeset_functions | wc -l)
 WVPASS [ $count -gt 1 ]
@@ -38,11 +24,4 @@ abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz() { :; }
 WVPASS [ $(_longest_function_name_length) -eq 52 ]
 unset -f abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
 
-WVSTART "_max_letterpress_width"
-WVPASS [ $(_max_letterpress_width) -gt 0 ]
-abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz() { :; }
-_max_letterpress_width
-WVPASS [ $(_max_letterpress_width) -eq 57 ]
-unset -f abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-
-WVSTART "_transcribe"
+WVSTART "_add_composure_file"
