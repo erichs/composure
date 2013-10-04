fa9087fe815ae6344c1f6496a17538d11a7feb3d
Erich Smith <heartquest@gmail.com>
2013-10-04 17:21:23 -0400

    Test _transcribe logic

--- test/t/plumbing.sh
+++ test/t/plumbing.sh
@@ -39,21 +39,91 @@ abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz() { :; }
 WVPASS [ $(_longest_function_name_length) -eq 52 ]
 unset -f abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
 
-pwd
 WVSTART "_add_composure_file"
 _add_composure_file one "$(pwd)/fixtures/one.inc" Add "first test function"
 (
   cd $COMPOSURE_DIR
   WVPASSEQ "$(git log --format=%B -n 1 HEAD)" "Add one: first test function"
 )
+WVPASSRC $?
 
 echo "second test function" | _add_composure_file two "$(pwd)/fixtures/two.inc" Add
 (
   cd $COMPOSURE_DIR
   WVPASSEQ "$(git log --format=%B -n 1 HEAD)" "Add two: second test function"
 )
+WVPASSRC $?
 
+WVSTART "_transcribe"
+_transcribe three "$(pwd)/fixtures/three.inc" Add "third test function"
+(
+  cd $COMPOSURE_DIR
+  WVPASSEQ "$(git log --format=%B -n 1 HEAD)" "Add three: third test function"
+)
+WVPASSRC $?
+
+rm -rf $COMPOSURE_DIR
+USE_COMPOSURE_REPO=0
+WVPASSEQ "$(_transcribe)" ""
+unset USE_COMPOSURE_REPO
+echo "n" | _transcribe | grep -q USE_COMPOSURE_REPO=0
+WVPASSRC $?
+echo "y" | _transcribe four "$(pwd)/fixtures/four.inc" Add "fourth" #| grep -q 'creating git repository'
+WVPASSRC $?
+(
+  cd $COMPOSURE_DIR
+  WVPASSEQ "$(git log --format=%B -n 1 HEAD)" "Add four: fourth"
+)
+WVPASSRC $?
 
 WVSTART "cleanup test env"
 rm -rf $COMPOSURE_DIR
 WVPASS [ ! -d $COMPOSURE_DIR ]
+
+# _transcribe ()
+#{
+#  typeset func="$1"
+#  typeset file="$2"
+#  typeset operation="$3"
+#  typeset comment="${4:-}"
+#
+#  if git --version >/dev/null 2>&1; then
+#    if [ -d "$COMPOSURE_DIR" ]; then
+#      _add_composure_file "$func" "$file" "$operation" "$comment"
+#    else
+#      if [ "$USE_COMPOSURE_REPO" = "0" ]; then
+#        return  # if you say so...
+#      fi
+#      printf "%s\n" "I see you don't have a $COMPOSURE_DIR repo..."
+#      typeset input=''
+#      typeset valid=0
+#      while [ $valid != 1 ]; do
+#        printf "\n%s" 'would you like to create one? y/n: '
+#        read input
+#        case $input in
+#          y|yes|Y|Yes|YES)
+#            (
+#              echo 'creating git repository for your functions...'
+#              mkdir "$COMPOSURE_DIR"
+#              cd "$COMPOSURE_DIR"
+#              git init
+#              echo "composure stores your function definitions here" > README.txt
+#              git add README.txt
+#              git commit -m 'initial commit'
+#            )
+#            # if at first you don't succeed...
+#            _transcribe "$func" "$file" "$operation"
+#            valid=1
+#            ;;
+#          n|no|N|No|NO)
+#            printf "%s\n" "ok. add 'export USE_COMPOSURE_REPO=0' to your startup script to disable this message."
+#            valid=1
+#          ;;
+#          *)
+#            printf "%s\n" "sorry, didn't get that..."
+#          ;;
+#        esac
+#      done
+#     fi
+#  fi
+#}
