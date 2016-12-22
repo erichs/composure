7de4304b01485f53b10c6da8fd800391e8965161
Erich Smith <heartquest@gmail.com>
2016-12-22 17:29:47 -0500

    Update LICENSE copyright

--- composure.sh
+++ composure.sh
@@ -540,7 +540,7 @@ _bootstrap_composure
 : <<EOF
 License: The MIT License
 
-Copyright © 2012, 2013 Erich Smith
+Copyright © 2012, 2016 Erich Smith
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in the Software
